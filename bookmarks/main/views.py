691da32b2917ceb4be0e9230dc9bb69c5761bdd3
JakeBrand <jabrand@ualberta.ca>
2015-02-25 11:56:31 -0700

    first working commit. Add link not yet working, not is tags working

---
+++ bookmarks/main/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.shortcuts import redirect
+
+from main.models import Link
+from main.models import Tag
+
+
+def index(request):
+    # Request the context of the reqeust
+    # The context contains information such as client's machine
+    context = RequestContext(request)
+
+    # Get all links
+    links = Link.objects.all()
+    # when this links object is passed, {%%} and {{}} is used to access them
+    return render_to_response('main/index.html', {'links': links}, context)
+
+
+def tags(request):
+    context = RequestContext(request)
+
+    # Get all tags
+    tags = Tag.objects.all()
+
+    return render_to_response('main/tags.html', {'tags': tags}, context)
+
+
+def tag(request, tag_name):
+    context = RequestContext(request)
+    the_tag = Tag.objects.get(name=tag_name)
+    links = the_tag.link_set.all()
+    return render_to_response('main/index.html', {'links':links, 'tag_name': '#' + tag_name}, context)
+
+def add_link(request):
+    context = RequestContext(request)
+    if(request.method == 'POST'):
+        url = request.POST.get("url", "")
+        tags = request.POST.get("tags", "")
+        title = request.POST.get("title", "")
+        #TODO: My code here
+    return redirect(index)
---
+++ v1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/cshome/jabrand/web1/lab6/2/v1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
